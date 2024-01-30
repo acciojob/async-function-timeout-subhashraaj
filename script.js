547f930bc8f38183a78c501956b3d06e814298c9
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-30 11:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+document.querySelector("#btn").addEventListener("click",async()=>{
+	const text = document.querySelector("#text").value;
+	const delay = document.querySelector("#delay").value;
+	if(text&&delay){
+		const myPromise = new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				resolve(text)	
+			}, delay)
+		});
+		let text2 = await myPromise;
+		if(text2){
+			document.querySelector("#output").innerText=text2;
+		}
+	};
+});
